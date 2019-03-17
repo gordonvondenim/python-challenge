7256d6d9122b17cba78ee595963b6900d9f3e09f
Jordan Whitney <jordanmwhitney@gmail.com>
2019-03-17 16:49:48 -0600

    updates to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,32 @@
 import os
 import csv
+import statistics
 
 #choose where to read
-budgetcsv = os.path.join('budget.csv')
+budget_csv = os.path.join('..','budget.csv')
+#check if file exists
+#os.path.exists('budget.csv') test returned true
+
+#read csv and print data
+with open(budget_csv, newline='') as budget:
+    reader = csv.reader(budget, delimiter = ",")
+    #store as lists
+    profit_loss = []
+    profit_loss_change = []
+    date = []
+    header = next(reader)
+   
+    for row in reader:
+        date.append(row[0]),
+        profit_loss.append(float(row[1]))
+    print("financial analysis:")
+    print("-------------------------------------")
+    print(f'Total Months: {len(date)}')
+    print(f'Total: ${sum(profit_loss)}')
+    print(f'Average Change: {statistics.mean(profit_loss)}')
+
+    
+
+
+
 
